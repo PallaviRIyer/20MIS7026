751d5755a1190f997ee9c2a4767ad46185a5fa2f
Pallavi.R.Iyer <pallavi.20mis7026@vitap.ac.in>
2021-02-27 20:11:07 +0530

    reverse of an array

---
+++ first.java
@@ -0,0 +1,14 @@
+//This is my first program in github
+public class resversearray {
+
+	public static void main(String[] args) {
+		int [] array= {1,2,3,4,5};
+		for(int i=4;i>=0;i--) {
+			System.out.print(array[i]);
+
+		}
+			
+	}
+
+}
+
