afb877f03f13e143d53b6f2adcd1745bce0f20e4
PallaviRIyer <79352525+PallaviRIyer@users.noreply.github.com>
2021-03-06 13:21:31 +0530

    Add files via upload
    pushed first.java

---
+++ first.java
@@ -0,0 +1,16 @@
+//This is my first program in github
+public class resversearray {
+
+	public static void main(String[] args) {
+		int [] array= {1,2,3,4,5};
+		for(int i=4;i>=0;i--) {
+			System.out.print(array[i]);
+System.out.println(array[1]);
+System.out.println(array[1]);
+
+		}
+		
+	}
+
+}
+
